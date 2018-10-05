009866e86fe37cf2540d7b186b4d628ebfa4c631
Juan varela <juanjose.varela3@gmail.com>
2018-10-05 10:54:21 -0500

    Add files via uploads
    Add files via uploads

---
+++ BIOIN_0_1/GUI/buildProyectGui.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+# Proyecto: BIOIN
+# Trabajo de Grado
+# Alejandro Valencia
+# Juan Jose Varela
+# Universidad del Valle
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font
+from panelGui import PanelWindow
+import webbrowser
+
+
+class BuildProyectWindow(ttk.Frame):
+
+    def __init__(self, build_proyect_window, main_window):
+
+        # Intancias y configuraciones de la ventana
+        self.build_proyect_window = build_proyect_window
+        self.main_window = main_window
+        super().__init__(self.build_proyect_window)
+        self.build_proyect_window.title("BIOIN - NUEVO PROYECTO")
+        self.build_proyect_window.geometry("500x400")
+        self.place(relwidth=1, relheight=1)
+        self.build_proyect_window.resizable(0, 0)
+        self.build_proyect_window.protocol("WM_DELETE_WINDOW", self.onClosing)
+
+        # menu de la pantalla de inicio
+        self.menubar = tk.Menu(self.build_proyect_window)
+        self.fileMenu = tk.Menu(self.menubar, tearoff=0)
+        self.fileMenu.add_command(label="Abrir...")
+
+        self.fileMenu.add_separator()
+
+        self.fileMenu.add_command(label="Salir", command=self.onClosing)
+        self.menubar.add_cascade(label="Proyecto", menu=self.fileMenu)
+
+        self.helpMenu = tk.Menu(self.menubar, tearoff=0)
+        self.helpMenu.add_command(label="Más Ayuda", command=self.openWebHelp)
+        self.helpMenu.add_command(label="Acerca de Bioin...", command=self.openWeb)
+        self.menubar.add_cascade(label="Ayuda", menu=self.helpMenu)
+
+        self.build_proyect_window.config(menu=self.menubar)
+
+        # definimos fuentes a usar
+        self.titleFont = font.Font(family="Times", size=14)
+
+        # Label principal
+        self.labelPrincipal = ttk.Label(self, text="Iniciar un nuevo proyecto", font=self.titleFont)
+        self.labelPrincipal.place(x=10, y=10)
+
+        # label secundario
+        self.labelSecond = ttk.Label(self, text="Seleccione los pasos:")
+        self.labelSecond.place(x=10, y=40)
+
+        # check buttons
+        self.step1Value = tk.BooleanVar(self)
+        self.step1 = ttk.Checkbutton(self, text="Paso 1", variable=self.step1Value)
+        self.step1.place(x=15, y=70)
+
+        self.step2Value = tk.BooleanVar(self)
+        self.step2 = ttk.Checkbutton(self, text="Paso 2", variable=self.step2Value)
+        self.step2.place(x=15, y=90)
+
+        self.step3Value = tk.BooleanVar(self)
+        self.step3 = ttk.Checkbutton(self, text="Paso 3", variable=self.step3Value)
+        self.step3.place(x=15, y=110)
+
+        self.step4Value = tk.BooleanVar(self)
+        self.step4 = ttk.Checkbutton(self, text="Paso 4", variable=self.step4Value)
+        self.step4.place(x=15, y=130)
+
+        self.step5Value = tk.BooleanVar(self)
+        self.step5 = ttk.Checkbutton(self, text="Paso 5", variable=self.step5Value)
+        self.step5.place(x=15, y=150)
+
+        # boton de inicio de proyecto
+        self.confirmButton = ttk.Button(self, text="Iniciar Proyecto", command=self.onStartProyect)
+        self.confirmButton.place(x=10, y=190)
+
+        # boton de regreso
+        self.confirmButton = ttk.Button(self, text="Regresar", command=self.onClosing)
+        self.confirmButton.place(x=200, y=190)
+
+    def onStartProyect(self):
+        stepsList = []
+        stepsList += [self.step1Value.get()]
+        stepsList += [self.step2Value.get()]
+        stepsList += [self.step3Value.get()]
+        stepsList += [self.step4Value.get()]
+        stepsList += [self.step5Value.get()]
+
+        print(stepsList)
+
+        self.build_proyect_window.destroy()
+        self.main_window.destroy()
+        new_window = tk.Tk()
+        panelwindow = PanelWindow(new_window, stepsList)
+        panelwindow.mainloop()
+
+    def onClosing(self):
+        self.top = tk.Toplevel(self.build_proyect_window)
+        self.top.title("Salir")
+
+        tk.Label(self.top, text="¿Está seguro?").grid(row=0, column=0, columnspan=2)
+
+        self.button1 = tk.Button(self.top, text="Si, deseo regresar.", command=self.salir)
+        self.button2 = tk.Button(self.top, text="Cancelar", command=self.cancelar)
+        self.button1.grid(row=1, column=0, padx=5, pady=5)
+        self.button2.grid(row=1, column=1, padx=5, pady=5)
+
+    def salir(self):
+        self.main_window.deiconify()
+        self.top.destroy()
+        self.build_proyect_window.destroy()
+
+    def cancelar(self):
+        self.top.destroy()
+
+    def openWeb(self):
+        url = "http://bioinformatica.univalle.edu.co/"
+        webbrowser.open(url)
+
+    def openWebHelp(self):
+        url = "http://bioinformatica.univalle.edu.co/"
+        webbrowser.open(url)
---
+++ BIOIN_0_1/GUI/mainGui.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+# Proyecto: BIOIN
+# Trabajo de Grado
+# Alejandro Valencia
+# Juan Jose Varela
+# Universidad del Valle
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font
+from buildProyectGui import BuildProyectWindow
+import webbrowser
+
+
+class MainWindow(ttk.Frame):
+
+    def __init__(self, main_window):
+
+        # Intancias y configuraciones de la ventana
+        self.main_window = main_window
+        super().__init__(self.main_window)
+        self.main_window.title("BIOIN")
+        self.main_window.geometry("500x400")
+        self.main_window.resizable(0, 0)
+        self.main_window.protocol("WM_DELETE_WINDOW", self.onClosing)
+        self.place(relwidth=1, relheight=1)
+
+        # menu de la pantalla de inicio
+        self.menubar = tk.Menu(self.main_window)
+        self.fileMenu = tk.Menu(self.menubar, tearoff=0)
+        self.fileMenu.add_command(label="Nuevo Proyecto", command=self.newProyectWindow)
+        self.fileMenu.add_command(label="Abrir...")
+
+        self.fileMenu.add_separator()
+
+        self.fileMenu.add_command(label="Salir", command=self.onClosing)
+        self.menubar.add_cascade(label="Proyecto", menu=self.fileMenu)
+
+        self.helpMenu = tk.Menu(self.menubar, tearoff=0)
+        self.helpMenu.add_command(label="Más Ayuda", command=self.openWebHelp)
+        self.helpMenu.add_command(label="Acerca de Bioin...", command=self.openWeb)
+        self.menubar.add_cascade(label="Ayuda", menu=self.helpMenu)
+
+        main_window.config(menu=self.menubar)
+
+        # Cargar la imagen del logo
+        self.logo = tk.PhotoImage(file="images/logo.png")
+        self.logo = self.logo.subsample(2)
+
+        # Label del Logo
+        self.logoLabel = ttk.Label(self, image=self.logo)
+        self.logoLabel.place(x=100, y=-100)
+
+        # Botones de la pantalla de inicio
+        self.startNewProyectButton = ttk.Button(self, text="Nuevo Proyecto", width=15, command=self.newProyectWindow)
+        self.startNewProyectButton.place(x=200, y=210)
+
+        self.loadProyectButton = ttk.Button(self, text="Cargar Proyecto", width=15)
+        self.loadProyectButton.place(x=200, y=240)
+
+        self.settingsButton = ttk.Button(self, text="Ajustes", width=15)
+        self.settingsButton.place(x=200, y=270)
+
+        self.infoButton = ttk.Button(self, text="Más Información", width=15, command=self.openWeb)
+        self.infoButton.place(x=200, y=300)
+
+    # Funciones de la interfaz
+    def newProyectWindow(self):
+        self.main_window.withdraw()
+        new_window = tk.Tk()
+        buildProyectWindow = BuildProyectWindow(new_window, self.main_window)
+        buildProyectWindow.mainloop()
+
+    def onClosing(self):
+        self.top = tk.Toplevel(self.main_window)
+        self.top.title("Salir")
+
+        tk.Label(self.top, text="¿Está seguro?").grid(row=0, column=0, columnspan=2)
+
+        self.button1 = tk.Button(self.top, text="Si, deseo salir.", command=self.salir)
+        self.button2 = tk.Button(self.top, text="Cancelar", command=self.cancelar)
+        self.button1.grid(row=1, column=0, padx=5, pady=5)
+        self.button2.grid(row=1, column=1, padx=5, pady=5)
+
+    def salir(self):
+        self.top.destroy()
+        self.main_window.destroy()
+
+    def cancelar(self):
+        self.top.destroy()
+
+    def openWeb(self):
+        url = "http://bioinformatica.univalle.edu.co/"
+        webbrowser.open(url)
+
+    def openWebHelp(self):
+        url = "http://bioinformatica.univalle.edu.co/"
+        webbrowser.open(url)
+
+
+# Main donde todo inicia
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MainWindow(root)
+    root.mainloop()
