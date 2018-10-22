ff1de9e5f3c5c2e5cbd31dd43e1c2b7b1b5610d2
Juan varela <juanjose.varela3@gmail.com>
2018-10-22 10:28:22 -0500

    Nueva Clase, permite crear objeto Proyect que permite gestionar informacion del proyecto que esta siendo realizado, serealizar y tener persistencia de la informacion trabajada en python.

---
+++ BIOIN_0_1/GUI/proyect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Proyecto: BIOIN
+# Trabajo de Grado
+# Alejandro Valencia
+# Juan Jose Varela
+# Universidad del Valle
+
+
+class Proyect():
+
+    def __init__(self, steps_list):
+        self.steps_list = steps_list
