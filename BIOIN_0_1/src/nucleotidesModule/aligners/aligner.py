e68b3b7a4ff68e2ac0787d11906b5b85b96938cc
Juan varela <juanjose.varela3@gmail.com>
2019-05-24 16:50:05 -0500

    Actualization

--- BIOIN_0_1/src/nucleotidesModule/aligners/aligner.py
+++ BIOIN_0_1/src/nucleotidesModule/aligners/aligner.py
@@ -18,8 +18,8 @@ class Aligner:
 
     def ejecutCommand(self):
         # guardamos en una variable el comando a ejecutar
-        comando = "./nucleotidesModule/aligners/clustalw2 -ALIGN -INFILE="
-        comando += self.inputfile + " -OUTFILE=" + self.outputfile + "/output"
+        comando = "./nucleotidesModule/aligners/blastn -query " + self.proyect.genomeRefRoute + \
+                  " -subject " + self.proyect.genomeRefRoute + " -out " + self.proyect.dirRoute + "/Homologia/output.txt"
 
         # convertimos el string en una lista para poder pasar de manera adecuada los comandos desde python
         args = sl.split(comando)
@@ -28,13 +28,23 @@ class Aligner:
         sp.call(args)
 
     def fileExist(self):
-        path = self.outputfile + "/output"
+        path = self.proyect.dirRoute + "/Homologia/output.txt"
         return os.path.exists(path)
 
     def ejecutCommandAlignBWA(self):
         # guardamos en una variable el comando a ejecutar
-        comando = "./nucleotidesModule/aligners/bwa/bwakit/run-bwamem -o " + self.proyect.dirRoute + "/Homologia/output"
-        comando += self.proyect.genomeRefRoute + self.proyect.dirRoute + "/Ensamblaje/output"
+        comando = "./nucleotidesModule/aligners/bwa/bwa index -p " + self.proyect.genomeRefRoute + " " + self.proyect.genomeRefRoute
+        # comando += self.proyect.genomeRefRoute + " " +   # self.proyect.dirRoute + "/Ensamblaje/output"
+
+        # convertimos el string en una lista para poder pasar de manera adecuada los comandos desde python
+        args = sl.split(comando)
+
+        # ejecutamos la función call de subprocess que permite ejecutar comandos desde la temrinal
+        sp.call(args)
+
+        # guardamos en una variable el comando a ejecutar
+        comando = "./nucleotidesModule/aligners/bwa/bwakit/run-bwamem -o " + self.proyect.dirRoute + "/Homologia/output "
+        comando += self.proyect.genomeRefRoute + " " + self.proyect.genomeRefRoute#self.proyect.dirRoute + "/Ensamblaje/output"
 
         # convertimos el string en una lista para poder pasar de manera adecuada los comandos desde python
         args = sl.split(comando)
